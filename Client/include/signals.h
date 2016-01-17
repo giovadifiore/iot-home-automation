c26235852635de8d582fa6f9181df79007fbb0a3
Giovanni Di Fiore <giovannidifiore@MacBook-Pro-di-Giovanni.local>
2016-01-17 10:50:46 +0100

    clean up

--- Client/include/signals.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIGNALS_H_
-#define SIGNALS_H_
-
-#include "stm32f4xx_helpers.h"
-
-void doSignalDoorStatusChanged(JsonFrame_TypeDef* outgoingFrame, const DOORStatus_TypeDef doorStatus);
-void doSignalUserBtnPressed(JsonFrame_TypeDef* outgoingFrame);
-
-#endif
--- README.md
+++
@@ -1,2 +0,0 @@
-# iot-home-automation
-A proof-of-concept of a Client-Server (NodeJS and C) implementation for domestic control and home automation
